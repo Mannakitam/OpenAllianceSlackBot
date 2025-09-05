13e07aa92debaa4748d61c4392ad1ed47437803c
Mannakitam <mayer.bergman@gmail.com>
2025-09-04 23:34:02 -0400

    started adding clickup support

--- clickUpAPI.js
+++ clickUpAPI.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const fs = require('node:fs/promises');
 
-
 const url = "https://api.clickup.com/api/v2/team/9011117189/task";
 const options = {
                 method: 'GET', 
@@ -11,10 +10,13 @@ const options = {
                         
                 }};
 
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => {
-    console.log('Data saved');
-    fs.writeFile('./temp.json', JSON.stringify(json, null, 2));
-    })
-  .catch(err => console.error(err)); 
+
+function cacheClickUpData() {
+    fetch(url, options)
+    .then(res => res.json())
+    .then(json => {
+        console.log('Data saved');
+        fs.writeFile('./temp.json', JSON.stringify(json, null, 2));
+        })
+    .catch(err => console.error(err)); 
+}
